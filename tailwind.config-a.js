172a5eadcacc9a05ce597c988e6ebe926ab8ed1f
Elton <elton@ingenie.com>
2020-10-04 13:22:51 +0100

    chore: multi brands build starter

---
+++ tailwind.config-a.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontSize: {
+      base: "10rem",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
---
+++ tailwind.config-b.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontSize: {
+      base: "1rem",
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
