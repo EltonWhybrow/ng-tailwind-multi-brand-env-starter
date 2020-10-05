272413b8b90bbbf9a7377807653a3cdc9cc075f0
Elton <elton@ingenie.com>
2020-10-05 10:11:07 +0100

    chore: renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'widesign-elton'`, () => {
+  it(`should have as title 'ng-tailwind-starter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('widesign-elton');
+    expect(app.title).toEqual('ng-tailwind-starter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('widesign-elton app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'ng-tailwind-starter app is running!'
+    );
   });
 });
